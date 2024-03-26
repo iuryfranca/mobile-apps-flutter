9dadb38f65b95f83be9165b2198f4b913782a35f
Iury França <iurygfranca@gmail.com>
2024-03-26 02:51:30 -0300

    feat: criando listagem de filmes com material Desing 3 ✨

---
+++ lib/MovieList.dart
@@ -0,0 +1,298 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:my_flutter_app/Movie.dart';
+import 'package:my_flutter_app/MovieDetail.dart';
+import 'package:dio/dio.dart';
+import 'package:my_flutter_app/main.dart';
+
+class MovieList extends StatelessWidget {
+  /// Method to get movies from the backend
+  // Future<List<Movie>> getMovies() async {
+  //   const String url =
+  //       'https://api.themoviedb.org/3/movie/popular?api_key=2b714e82ab5108ff8b963154afd167ea&language=pt-BR';
+  //   var httpClient = HttpClient();
+  //   try {
+  //     // Make the call
+  //     var request = await httpClient.getUrl(Uri.parse(url));
+  //     var response = await request.close();
+  //     if (response.statusCode == HttpStatus.ok) {
+  //       var json = await response.transform(utf8.decoder).join();
+  //       return createMovieList(json);
+  //     } else {
+  //       print("Failed http call.");
+  //     }
+  //   } catch (exception) {
+  //     print(exception.toString());
+  //   }
+  //   return [];
+  // }
+
+  Future<List<Movie>> getMoviesWithDio() async {
+    const String url =
+        'https://api.themoviedb.org/3/movie/popular?api_key=2b714e82ab5108ff8b963154afd167ea&language=pt-BR';
+
+    final dio = Dio();
+    try {
+      // Make the call
+      var request = await dio.get(url);
+      if (request.statusCode == HttpStatus.ok) {
+        return createMovieList(request.toString());
+      } else {
+        print("Failed http call.");
+      }
+    } catch (exception) {
+      print(exception.toString());
+    }
+    return [];
+  }
+
+  /// Method to parse information from the retrieved data
+  List<Movie> createMovieList(String resultString) {
+    List results = getResultsList(resultString);
+
+    List<Movie> list = [];
+    for (int i = 0; i < results.length; i++) {
+      list.add(createMovieObject(results[i]));
+    }
+    return list;
+  }
+
+  /// Method to create a movie object.
+  Movie createMovieObject(objectItem) {
+    String title = objectItem["title"];
+    String posterPath = objectItem["poster_path"];
+    String backdropImage = objectItem["backdrop_path"];
+    String originalTitle = objectItem["original_title"];
+    double voteAverage = objectItem["vote_average"];
+    String overview = objectItem["overview"];
+    String releaseDate = objectItem["release_date"];
+
+    return Movie(title, posterPath, backdropImage, originalTitle, voteAverage,
+        overview, releaseDate);
+  }
+
+  List getResultsList(String resultString) {
+    // Decode the json response
+    var data = json.decode(resultString);
+    // Get the result list
+    return data["results"];
+  }
+
+  List<Widget> createMovieCardItem(List<Movie>? movies, BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    final TextTheme textTheme = themeData.textTheme;
+
+    // Children list for the list.
+    List<Widget> listElementWidgetList = [];
+    if (movies != null) {
+      var lengthOfList = movies.length;
+      for (int i = 0; i < lengthOfList; i++) {
+        Movie movie = movies[i];
+        // Image URL
+        var imageURL = "https://image.tmdb.org/t/p/w500${movie.posterPath}";
+
+        var listItem = Hero(
+          tag: Image.network(imageURL),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(8),
+                topRight: Radius.circular(8),
+              ),
+              image: DecorationImage(
+                image: Image.network(imageURL).image,
+                fit: BoxFit.contain,
+                alignment: Alignment.topCenter,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    movie.title,
+                    style: textTheme.titleMedium,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+
+        listElementWidgetList.add(listItem);
+      }
+    }
+    return listElementWidgetList;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: getMoviesWithDio(),
+        builder: (BuildContext context, AsyncSnapshot<List<Movie>> snapshot) {
+          List<Movie>? movies = snapshot.data;
+          if (snapshot.hasData == false) {
+            // Shows progress indicator until the data is load.
+            return const MaterialApp(
+                home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ));
+          }
+          // Shows the real data with the data retrieved.
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Listagem de Filmes'),
+              scrolledUnderElevation: 4.0,
+              shadowColor: Theme.of(context).shadowColor,
+              leading: const Center(
+                child: CircleAvatar(
+                  radius: 16,
+                  child: Icon(Icons.person),
+                ),
+              ),
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(64.0),
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  padding:
+                      const EdgeInsets.only(left: 16, bottom: 10, right: 16),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      isDense: true,
+                      hintText: 'Procurar filme...',
+                      filled: true,
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.search_rounded),
+                    ),
+                  ),
+                ),
+              ),
+              actions: [
+                // IconButton(
+                //   onPressed: () {},
+                //   style:
+                //       enabledFilledButtonStyle(false, Theme.of(context).colorScheme),
+                //   icon: const Icon(Icons.sort_rounded),
+                // ),
+                PopupMenuButton<AppMenu>(
+                  itemBuilder: (BuildContext context) =>
+                      <PopupMenuEntry<AppMenu>>[
+                    const PopupMenuItem<AppMenu>(
+                      value: AppMenu.about,
+                      child: Text('About us'),
+                    ),
+                    const PopupMenuItem<AppMenu>(
+                      value: AppMenu.privacy,
+                      child: Text('Privacy Policy'),
+                    ),
+                    const PopupMenuItem<AppMenu>(
+                      value: AppMenu.settings,
+                      child: Text('Settings'),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            body: GridView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              itemCount: movies?.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 12 / 22,
+              ),
+              itemBuilder: (context, index) {
+                final List listMovies = createMovieCardItem(movies, context);
+
+                return Card(
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  MovieDetail(movies![index])));
+                    },
+                    child: listMovies[index],
+                  ),
+                );
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () async => _shoppingCartDialog(context),
+              child: const Icon(Icons.sort_rounded),
+            ),
+          );
+        });
+  }
+
+  Future<void> _shoppingCartDialog(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Filtros'),
+          icon: const Icon(Icons.sort_rounded),
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Divider(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                textDirection: TextDirection.ltr,
+                children: <Widget>[
+                  Text('1 x Lorem Ipsum'),
+                  Text('\$9.99'),
+                  Text('1 x Lorem Ipsum'),
+                  Text('\$9.99'),
+                  Text('1 x Lorem Ipsum'),
+                  Text('\$9.99'),
+                ],
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Limpar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Filtrar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  ButtonStyle enabledFilledButtonStyle(bool selected, ColorScheme colors) {
+    return IconButton.styleFrom(
+      foregroundColor: selected ? colors.onPrimary : colors.primary,
+      backgroundColor: selected ? colors.primary : colors.surfaceVariant,
+      disabledForegroundColor: colors.onSurface.withOpacity(0.38),
+      disabledBackgroundColor: colors.onSurface.withOpacity(0.12),
+      hoverColor: selected
+          ? colors.onPrimary.withOpacity(0.08)
+          : colors.primary.withOpacity(0.08),
+      focusColor: selected
+          ? colors.onPrimary.withOpacity(0.12)
+          : colors.primary.withOpacity(0.12),
+      highlightColor: selected
+          ? colors.onPrimary.withOpacity(0.12)
+          : colors.primary.withOpacity(0.12),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,188 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/MovieList.dart';
 
-void main() => runApp(const MyApp());
+enum AppMenu {
+  about,
+  privacy,
+  settings,
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const MovieApp());
+
+class MovieApp extends StatelessWidget {
+  const MovieApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'The Last Of Us Part II - Remastered';
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.black,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
-        ),
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.green[700],
       ),
-      title: appTitle,
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text(appTitle),
-          ),
-          body: const SingleChildScrollView(
-            child: Column(children: [
-              ImageSection(
-                  image: 'assets/The-Last-of-Us-Parte-II-Remasterizado-2.jpg'),
-              TitleSection(name: 'Video Game', location: 'Naughty Dog'),
-              ButtonSection(),
-              TextSection(
-                  description:
-                      'The Last of Us Part II é um jogo eletrônico de ação-aventura e sobrevivência desenvolvido pela Naughty Dog e publicado pela Sony Interactive Entertainment. É a sequência de The Last of Us, lançado em 2013 para PlayStation 3. O jogo foi lançado para PlayStation 4 em 19 de junho de 2020. A história se passa cinco anos após os eventos do primeiro jogo e segue Ellie e Abby, cujos destinos se entrelaçam em um ciclo de vingança e perdão. O jogador controla Ellie, uma jovem que embarca em uma jornada por Seattle, Washington, em busca de vingança contra um grupo misterioso. O jogo é jogado a partir de uma perspectiva de terceira pessoa e os jogadores podem usar armas de fogo, armas improvisadas e furtividade para se defender contra humanos e criaturas hostis. O jogo também possui elementos de sobrevivência, como coleta de recursos, artesanato e resolução de quebra-cabeças.'),
-            ]),
-          )),
-    );
-  }
-}
-
-class TitleSection extends StatelessWidget {
-  const TitleSection({
-    super.key,
-    required this.name,
-    required this.location,
-  });
-
-  final String name;
-  final String location;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  location,
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.orange[500],
-          ),
-          const Text('95'),
-        ],
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.green[700],
       ),
+      home: MovieList(),
     );
-  }
-}
-
-class ButtonWithText extends StatelessWidget {
-  const ButtonWithText({
-    super.key,
-    required this.color,
-    required this.icon,
-    required this.label,
-  });
-
-  final Color color;
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Padding(
-          padding: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ButtonSection extends StatelessWidget {
-  const ButtonSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final Color color = Colors.black87;
-    return SizedBox(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ButtonWithText(
-            color: color,
-            icon: Icons.call,
-            label: 'Ligar',
-          ),
-          ButtonWithText(
-            color: color,
-            icon: Icons.near_me,
-            label: 'Endereço',
-          ),
-          ButtonWithText(
-            color: color,
-            icon: Icons.share,
-            label: 'Compartilhar',
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class TextSection extends StatelessWidget {
-  const TextSection({
-    super.key,
-    required this.description,
-  });
-
-  final String description;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        description,
-        softWrap: true,
-      ),
-    );
-  }
-}
-
-class ImageSection extends StatelessWidget {
-  const ImageSection({super.key, required this.image});
-
-  final String image;
-
-  @override
-  Widget build(BuildContext context) {
-    return Image.asset(image, fit: BoxFit.contain);
   }
 }
